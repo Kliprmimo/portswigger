47908032f729b9865924970af9679a90b411972d
Grzegorz Dziechciarz <grzes.dziechciarz@gmail.com>
2023-10-30 16:42:17 +0100

    deleted not nessecary element from header

--- login_template.py
+++ login_template.py
@@ -9,7 +9,7 @@ session_key = ''
 path= '/login'
 
 HEADERS = {'Host': url,
-            'Cookie':'session='+session_key+'; verify=carlos',
+            'Cookie':'session='+session_key,
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.5',
             'Accept-Encoding': 'gzip, deflate, br',
